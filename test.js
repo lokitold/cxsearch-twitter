32538b8be7b6f55d9e63560d77b9cad92fc82ee8
victor Rojas <victor.rojas@ec.pe>
2015-04-08 17:41:15 -0500

    testjs

---
+++ test.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express(),
+server = require('http').createServer(app),
+io = require('socket.io').listen(server);
+
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  //response.send('Hello World!');
+  response.sendfile(__dirname + '/indextest.html');
+});
+
+server.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
